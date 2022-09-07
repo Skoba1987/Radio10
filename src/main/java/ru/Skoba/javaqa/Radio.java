1912eed801a14c26d1fe8a8f1164630cb743d9ac
Skoba Kristina <kfisher@mail.ru>
2022-09-07 12:16:08 +0300

    second

---
+++ src/main/java/ru/Skoba/javaqa/Radio.java
@@ -0,0 +1,67 @@
+package ru.Skoba.javaqa;
+
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+
+    public int getCurrentStation() {
+
+        return currentStation;
+    }
+
+    public int getCurrentVolume() {
+
+        return currentVolume;
+    }
+
+    public void setCurrentStation(int newCurrentStation) {
+        if (newCurrentStation < 0) {
+            return;
+        }
+        if (newCurrentStation > 9) {
+            return;
+        }
+        currentStation = newCurrentStation;
+    }
+
+    public void nextStation() {
+        if (currentStation >= 9) {
+            currentStation = 0;
+        } else {
+            currentStation = currentStation + 1;
+        }
+    }
+
+    public void prevStation() {
+        if (currentStation <= 0) {
+            currentStation = 9;
+        } else {
+            currentStation = currentStation - 1;
+        }
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume >= 0 & newCurrentVolume <= 10) {
+            currentVolume = newCurrentVolume;
+        }
+    }
+
+    public void volumeUp() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+        if (currentVolume >= 10) {
+            currentVolume = 10;
+        }
+    }
+
+    public void volumeDown() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+        if (currentVolume <= 0) {
+            currentVolume = 0;
+        }
+    }
+
+}
---
+++ src/test/java/ru/Skoba/javaqa/TestRadio.java
@@ -0,0 +1,94 @@
+package ru.Skoba.javaqa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class TestRadio {
+    Radio radio = new Radio();
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/Station.csv")
+    public void shouldSetCurrentStation(int currentStation, int expected) {
+
+        radio.setCurrentStation(currentStation);
+
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "3,4",
+            "9,0",
+            "0,1"
+    })
+    public void shouldSetNextStation(int currentStation, int expected) {
+
+        radio.setCurrentStation(currentStation);
+        radio.nextStation();
+
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0,9",
+            "5,4",
+            "9,8"
+    })
+    public void shouldSetPrevStation(int currentStation, int expected) {
+
+        radio.setCurrentStation(currentStation);
+        radio.prevStation();
+
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/Volume.csv")
+    public void shouldSetCurrentVolume(int currentVolume, int expected) {
+
+        radio.setCurrentVolume(currentVolume);
+
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "5,6",
+            "10,10",
+            "11,1"
+    })
+    public void shouldSetVolumeUp(int currentVolume, int expected) {
+        radio.setCurrentVolume(currentVolume);
+        radio.volumeUp();
+
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "8,7",
+            "0,0",
+            "-1,0"
+    })
+    public void shouldSetVolumeDown(int currentVolume, int expected) {
+        radio.setCurrentVolume(currentVolume);
+        radio.volumeDown();
+
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
